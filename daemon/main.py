4cc6382d8818beb56c0c7392b18fc7f0e9e6da79
Quellus <quellus7@gmail.com>
2022-07-26 06:26:17 +0100

    Reorganize directory structure for webapp

--- daemon/main.py
+++ daemon/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, status
 from fastapi_restful.tasks import repeat_every
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 from controller import Controller
@@ -8,6 +9,18 @@ import models
 app = FastAPI()
 controller = Controller()
 
+origins = [
+  "*",
+  "https://raspberrypi.local"
+]
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.on_event("startup")
 @repeat_every(seconds=10)
@@ -15,9 +28,9 @@ async def drive_status():
   controller.drive_status()
 
 
-@app.get("/", response_model=models.Status)
+@app.get("/", response_model=object)
 async def root() -> dict:
-  return controller.get_status()
+  return {"status": controller.get_status()}
 
 
 @app.get("/temperature")
