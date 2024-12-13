6f3646713db41b50bf9272d8c2f9424d2c7dc327
quellus <quellus7@gmail.com>
2024-12-12 23:17:34 -0700

    Move AHT20 and DHT22 code to the same Arduino file

---
+++ TemperatureSensor/Config.h
@@ -0,0 +1,10 @@
+#define SENSOR_TYPE true // true for AHT20, false for DHT22
+
+#define SERVER_IP "ip:port"
+#define DHTPIN 14     // Digital pin connected to the DHT sensor 
+#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+
+#ifndef STASSID
+#define STASSID "SSID"
+#define STAPSK "Password"
+#endif
